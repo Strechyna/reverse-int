7a7cf14c26ef79db5e38262ab6707219896a95c2
Valeryia Strechyna <brouillasse@gmail.com>
2021-08-16 16:49:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (isNaN(n) || n === null || n === undefined) return 0;
+
+    const positiveN = Math.abs(n);
+    const arrayOfChars = Array.from(String(positiveN));
+    const reversString = arrayOfChars.reverse().join('');
+    return Number(reversString);
 }
